95434d1ee2dee327e0309a70e7347b2a9a61e2c8
Mourey jerome <jerome@everycheck.fr>
2019-11-13 13:13:45 +0100

    Opening empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate minifb;
+use minifb::{Key, KeyRepeat, WindowOptions, Window};
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 480;
+const TITLE: &str = "TUI Example - ESC to exit";
+
 fn main() {
-    println!("Hello, world!");
+    let window = Window::new(TITLE,WIDTH,HEIGHT,WindowOptions::default());
+    let mut window = window.unwrap_or_else(|e| {panic!("{}", e);});
+
+    let buffer =[ 0u32 ; WIDTH*HEIGHT];
+
+    while window.is_open() && !window.is_key_down(Key::Escape)
+    {
+        window.update_with_buffer(&buffer).unwrap();
+    }
 }
